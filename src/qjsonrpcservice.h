2f435bf45045590ec301f00c2d69b3b0c974670d
Matt Broadstone <mbroadst@gmail.com>
2013-03-06 13:37:02 -0500

    updated license to LGPL 2.1

--- src/qjsonrpcservice.h
+++ src/qjsonrpcservice.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012-2013 Matt Broadstone
+ * Contact: http://bitbucket.org/devonit/qjsonrpc
+ *
+ * This file is part of the QJsonRpc Library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 #ifndef QJSONRPCSERVICE_H
 #define QJSONRPCSERVICE_H
 
@@ -91,11 +107,11 @@ public Q_SLOTS:
     QJsonRpcServiceReply *sendMessage(const QJsonRpcMessage &message);
 //  void sendMessage(const QList<QJsonRpcMessage> &bulk);
     QJsonRpcMessage invokeRemoteMethodBlocking(const QString &method, const QVariant &arg1 = QVariant(),
-                                              const QVariant &arg2 = QVariant(), const QVariant &arg3 = QVariant(),
-                                              const QVariant &arg4 = QVariant(), const QVariant &arg5 = QVariant(),
-                                              const QVariant &arg6 = QVariant(), const QVariant &arg7 = QVariant(),
-                                              const QVariant &arg8 = QVariant(), const QVariant &arg9 = QVariant(),
-                                              const QVariant &arg10 = QVariant());
+                                               const QVariant &arg2 = QVariant(), const QVariant &arg3 = QVariant(),
+                                               const QVariant &arg4 = QVariant(), const QVariant &arg5 = QVariant(),
+                                               const QVariant &arg6 = QVariant(), const QVariant &arg7 = QVariant(),
+                                               const QVariant &arg8 = QVariant(), const QVariant &arg9 = QVariant(),
+                                               const QVariant &arg10 = QVariant());
     QJsonRpcServiceReply *invokeRemoteMethod(const QString &method, const QVariant &arg1 = QVariant(),
                                              const QVariant &arg2 = QVariant(), const QVariant &arg3 = QVariant(),
                                              const QVariant &arg4 = QVariant(), const QVariant &arg5 = QVariant(),
